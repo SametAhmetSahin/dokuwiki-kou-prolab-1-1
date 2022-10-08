fac6b68716e8d557f067b1cd78e4c00520bacd52
Samet Ahmet Şahin <sametahmetsahin@protonmail.com>
2022-10-08 18:44:00 +0300

    Added PushBackUnique, tags vector, changed DirectoryScanner to use PushBackUnique, added ReadFile, added ConvertTagnameToFile (Currently kind of broken), added FindOrphanTags, added W.I.P. FindTags

--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 #include <iostream>
 #include <filesystem>
 #include <vector>
+#include <fstream>
+#include <cstring>
+#include <map>
 #include <algorithm>
 
 namespace fs = std::filesystem;
@@ -9,8 +12,18 @@ using namespace std;
 
 vector<string> files;
 vector<string> directories;
+vector<string> tags;
 
 // snake_case for variables, PascalCase for functions
+
+int PushBackUnique(vector<string>& vector, string element) { // İlk argüman olarak vektörü, ikinci argüman olarak eklenecek elemanı alır. Vektör string tipindedir.
+	bool exists = 0;
+	for (string& i : vector) {
+		if (i == element) exists = 1;
+	}
+	if (!exists) vector.push_back(element);
+	return 0;
+}
 
 int DirectoryScanner(string path) {
 	for (const auto& entry : fs::directory_iterator(path))
@@ -18,30 +31,143 @@ int DirectoryScanner(string path) {
 		
 		//cout << entry.path() << endl;
 		if (entry.path().string().find(".") != string::npos) {
-			bool exists = 0;
+			
 			if (entry.path().string().find(".txt") != string::npos) {
-				for (string& i : files) 
-				{
-					if (i == entry.path().string()) exists = 1;
-				}
-				if (!exists) files.push_back(entry.path().string());
+				PushBackUnique(files, entry.path().string());
 			}
 		}
 		else { 
-			bool exists = 0;
-			for (string& i : directories) 
-			{
-				if (i == entry.path().string()) exists = 1;
-			}
-			if (!exists) directories.push_back(entry.path().string());
-				
+			PushBackUnique(directories, entry.path().string());
 		}
 	}
 	return 0;
 }
 
-int ReadFile(string path) {
+string ReadFile(string path) {
+	ifstream file;
 
+	file.open(path);
+	string content = "";
+
+	string templine;
+
+	if (file.is_open()) {
+		while (getline(file, templine)) {
+				content += templine + "\n";
+		}
+	}
+	
+	return content;
+}
+
+string ConvertTagnameToFile(string tagname) {
+	map<char, char> table;
+	table['ö'] = 'o';
+	table['ü'] = 'u';
+	table['ğ'] = 'g';
+	table['ş'] = 's';
+	table['ç'] = 'c';
+	table['ı'] = 'i';
+
+	table['Ö'] = 'O';
+	table['Ü'] = 'U';
+	table['Ğ'] = 'G';
+	table['Ş'] = 'S';
+	table['Ç'] = 'C';
+	table['İ'] = 'I';
+	
+	table[' '] = '_';
+	
+
+	map<char, char> :: iterator iter;
+
+	string tagname_converted = tagname;
+	size_t pos;
+	for (auto element:table) {
+		replace(tagname_converted.begin(), tagname_converted.end(), element.first, element.second);
+	}
+	
+	
+	return tagname_converted;
+}
+
+vector<string> FindOrphanTags () {
+
+	vector<string> orphantags;
+
+	for (string& filename : files) {
+		for (string& tagname : tags) {
+
+			vector<string> splitted;
+			char chararray[filename.length()+1];
+			strcpy(chararray, filename.c_str());
+			
+			char* token = strtok(chararray, "/");
+			while (token != NULL) {
+				string stringtoken;
+				stringtoken = token;
+				splitted.push_back(stringtoken);
+				token = strtok(NULL, "/");
+			}
+			
+			vector<string> filetxts;
+
+			int counter = 0;
+			for (string& filetxt : splitted) {
+				counter++;
+				counter %= 3;
+				if (counter == 0) filetxts.push_back(filetxt);
+				
+			}
+
+			vector<string> basenames;
+
+			for (string& filetxt: filetxts) {
+				char chararray[filetxt.length()+1];
+				strcpy(chararray, filetxt.c_str());
+			
+				char* token = strtok(chararray, ".");
+				string tokenstring = token;
+				basenames.push_back(tokenstring);
+			}
+
+			for (string& basename: basenames) {
+				//cout << basename << endl;
+				string tagname_converted = ConvertTagnameToFile(basename);
+				cout << "Converted tagname:" << tagname_converted << endl;
+			}
+
+			
+			
+			
+		}
+	}
+
+	return orphantags;
+}
+
+int FindTags(string content)
+{
+	// string.find() ile [[ aranır, ardından indisi ondan sonraki ]] aranır, [['ın bulunduğu karakterden itibaren okunmaya başlanır, 
+	// arada [ veya ] varsa arama iptal edilir, bir sonraki [[ aranır 
+	int tag_start_index;
+	int tag_end_index;
+
+	while(content.find("]]") != string::npos/*tag_end_index < content.length()*/)
+	{
+		tag_start_index = content.find("[[");
+		tag_end_index = content.find("]]");
+
+		char tag[tag_end_index - tag_start_index + 2];
+		memcpy(tag, (void*)(tag_start_index + (int*)&content), sizeof(tag) - 1);
+		tag[tag_end_index - tag_start_index + 1] = '\0';
+		string tagstring = tag;
+		PushBackUnique(tags, tagstring);
+
+		break;
+	}
+
+	return 0;
 }
 
 int main(int argc, char *argv[]) 
@@ -60,9 +186,19 @@ int main(int argc, char *argv[])
 	{
 		cout << i << endl;
 	}
+
+	//FindTags(ReadFile("Üniversite/Dersler/Programlama_I.txt"));
+
+	for (string& tag : tags) {
+		cout << tag << endl;
+	}
 	
+	//cout << ReadFile("Üniversite/Dersler/Programlama_I.txt");
+	tags.push_back("[[Programlama I]]");
 
+	vector<string> orphantags = FindOrphanTags();
+
+	
 	return 0;
+	
 }
-
-
