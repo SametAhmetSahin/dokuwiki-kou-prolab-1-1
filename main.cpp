b433e0531c5abe46e2da1714ab14e12fed7c61f7
Samet Ahmet Şahin <sametahmetsahin@protonmail.com>
2022-10-10 00:26:53 +0300

    Added LineLengthsOfFile, FindLinesOfCharPositions, FileLineCounter and SearchForWord functions

--- main.cpp
+++ main.cpp
@@ -224,35 +224,126 @@ vector <string> FindTags(string content, bool global_vector = true) {
 	return local_tags_vector;
 }
 
+vector<int> LineLengthsOfFile(string filepath) {
+	vector <int> linelengths;
+	string line;
+	ifstream file(filepath);
+    if(file.is_open()) {
+        while(!file.eof()) {
+            getline(file,line);
+			//cout << line.length() << endl;
+            linelengths.push_back(line.length());
+        }
+        file.close();
+    }
+	return linelengths;
+}
+
+vector<int> FindLinesOfCharPositions(vector<int> linelengths, vector<int> charpositions) {
+	vector <int> lines;
+	//cout << "Char positions size" << charpositions.size() << endl;
+	for (int& pos : charpositions) {
+		cout << "Starting new line lookup" << endl;
+		int val = pos; 
+		int linecount = 1;
+		for (int& length : linelengths) {
+			//cout << "val is: " << val << ", deducting " << length << " from it" << endl;
+			val -= length;
+			if (val < 0) {
+				lines.push_back(linecount);
+				break;
+			}
+			else {
+				linecount++;
+			}
+		}
+	}
+	return lines;
+}
+
+int FileLineCounter(string filepath) {
+	int count = 0;
+	string line;
+	ifstream file(filepath);
+    if(file.is_open()) {
+        while(!file.eof()) {
+            getline(file,line);
+            count++;
+        }
+        file.close();
+    }
+	return count - 1;
+}
+
+vector<string> SearchForWord(string word) {
+	bool istag = false;
+
+	
+	for (string& tag : tags) {
+		if (word == tag) {
+			istag = true;
+			cout << word << " is tag!";
+		}
+	}
+	
+	for (string& file : files) {
+		vector<int> foundpositions;
+		int pos = 0;
+		//cout << "Reading " << file << endl;
+		string content = ReadFile(file);
+
+		
+		while (pos < content.length() && (content.find(word) != string::npos)) {
+			pos = content.find(word, pos+1);
+			if (pos != -1) foundpositions.push_back(pos);
+		}
+
+		//if (*(foundpositions.end()) == -1) foundpositions.pop_back(); // -1'i yok eder.
+
+		//cout << "There are " << FileLineCounter(file) << " lines in file " << file << endl;
+		//cout << " found positions size is " << foundpositions.size() << " for word " << word << " for file " << file << endl;
+		/*
+		for (int& pos : foundpositions) {
+			cout << "Foundpos " << pos << endl; 
+		} */
+		
+		vector<int> lines = FindLinesOfCharPositions(LineLengthsOfFile(file), foundpositions);
+
+		//cout << "lines size: " << lines.size() << endl;
+
+		
+		for (int& line : lines) {
+			cout << "Found word " << word << " at line " << line << " at file " << file << endl;
+		}
+	}
+	
+}
 
 int main(int argc, char *argv[]) 
 {
-
-	
-	setlocale(LC_ALL, "");
-
 	DirectoryScanner("Üniversite");
 	
-	cout << "Directories" << endl << endl;
+	//cout << "Directories" << endl << endl;
 	for (string& i : directories) 
 	{
-		cout << i << endl;
+		//cout << i << endl;
 		DirectoryScanner(i);
 	}
 
-	cout << endl << endl << "Files" << endl << endl;
+	//cout << endl << endl << "Files" << endl << endl;
 	for (string& i : files) 
 	{
-		cout << i << endl;
+		//cout << i << endl;
 		FindTags(ReadFile(i));
 	}
 
 	//FindTags(ReadFile("Üniversite/Dersler/Programlama_I.txt"));
-	
+	/*
 	cout << endl << endl << "Tags" << endl;
 	for (string& tag : tags) {
 		cout << tag << endl;
 	}
+	*/
 	
 	
 
@@ -264,6 +355,7 @@ int main(int argc, char *argv[])
 	vector<string> orphantags = FindOrphanTags();
 	*/
 	
+	SearchForWord("Programlama I");
+
 	return 0;
-	
 }
